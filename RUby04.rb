a98274050500b58ee33b70f3e2743ca0cddfcd5b
prokeds <kdmmikata44@gmail.com>
2018-10-07 01:52:25 +0900

    4th commit

--- RUby04.rb
+++ RUby04.rb
@@ -3,10 +3,25 @@ class People
 end
 #2Peopleクラスに「Peopleのインスタンスが作られました」
 #と出力するコンストラクタを定義してください。
+class People
+  def initialize
+    p "Peopleのインスタンスが作られました"
+  end
+end
 
+people = People.new
 #3Peopleクラスに「私はPeopleクラスです」
 #と出力するクラスメソッドを定義してください。
+
+class People
+  def self.insta
+    p "私はPeopleクラスです"
+  end
+end
+
+People.insta
 #4Peopleクラスに、インスタンス変数「@name」を定義し、
+
 #「attr_accesor」メソッドでアクセスできるようにしてください。
 #5Peopleクラスを継承して、SuperPeopleクラスを作成してください。
 #6SuperPeopleクラスに「私は目からビームが出せます」
